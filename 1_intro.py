5a629f62768e4eba1803548d073d84c66235cd98
Azril-rahman-s <simamoraazril16@gmail.com>
2024-09-03 12:34:23 +0700

    perkuliahan 1

---
+++ 1_intro.py
@@ -0,0 +1,29 @@
+# Deklarasi Variabel
+a = float(10.5)
+b = 5
+
+print(a)
+print(b)
+
+# operasi -> +(tamabah),-(minus),/(bagi),*(kali),%->modulo(pembagian bersisa)
+penjumlahan = a + b
+perkalian = a*b
+pengurangan = a-b
+pembagian = a/b
+module = a%b
+perpangkatan = a**b
+
+print(penjumlahan)
+
+print(pengurangan)
+
+print(perkalian)
+
+print(pembagian)
+
+print(module)
+
+print(perpangkatan)
+
+print('hello world')
+
